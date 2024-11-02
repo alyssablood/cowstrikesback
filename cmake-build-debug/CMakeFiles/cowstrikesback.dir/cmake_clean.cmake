acc22840ef4d332735036ec0e8e7a119eacb423b
Lutfiyah Mohammed <lutfinawaz6.com>
2024-11-02 19:12:38 -0400

    new changes

---
+++ cmake-build-debug/CMakeFiles/cowstrikesback.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cowstrikesback.dir/cowsay.cpp.obj"
+  "CMakeFiles/cowstrikesback.dir/cowsay.cpp.obj.d"
+  "cowstrikesback.exe"
+  "cowstrikesback.exe.manifest"
+  "cowstrikesback.pdb"
+  "libcowstrikesback.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cowstrikesback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
